16cd964c46f301f6a4ac929309be1b6506472822
Zac Paulson <zpaulson001@gmail.com>
2024-04-06 13:31:27 -0700

    Create layout component

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,9 @@
+import Layout from '~/components/layout';
+
 export default function Index() {
   return (
-    <>
+    <Layout>
       <h1 className="text-3xl font-bold underline">look out world 👀</h1>
-    </>
+    </Layout>
   );
 }
