2500e15adea70dfd9813f9031e2a7532d6c3904a
Zac Paulson <zpaulson001@gmail.com>
2024-04-11 10:49:40 -0700

    DB updates

---
+++ prisma/migrations/20240411162437_/migration.sql
@@ -0,0 +1,21 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `url` on the `Message` table. All the data in the column will be lost.
+  - Added the required column `path` to the `Message` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Message" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "path" TEXT NOT NULL,
+    "authorId" TEXT NOT NULL,
+    CONSTRAINT "Message_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Message" ("authorId", "createdAt", "id") SELECT "authorId", "createdAt", "id" FROM "Message";
+DROP TABLE "Message";
+ALTER TABLE "new_Message" RENAME TO "Message";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
