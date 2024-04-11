e0f342a0d50b567ce9cf6887bc045d884de98730
Zac Paulson <zpaulson001@gmail.com>
2024-04-11 10:52:48 -0700

    Drawing pad testing

--- app/routes/playground.tsx
+++ app/routes/playground.tsx
@@ -1,10 +1,17 @@
-import Chat from '~/components/chat';
+import { ActionFunctionArgs } from '@remix-run/node';
+import DrawingPad from '~/components/drawingPad';
 import Layout from '~/components/layout';
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  console.log(formData.get('drawing'));
+  return null;
+}
 
 export default function PlaygroundPage() {
   return (
     <Layout className="flex flex-col justify-center items-center">
-      <Chat />
+      <DrawingPad />
     </Layout>
   );
 }
