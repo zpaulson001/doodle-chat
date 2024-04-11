83b316051504f8542fa486d71a9e8845608083cf
Zac Paulson <zpaulson001@gmail.com>
2024-04-11 10:52:20 -0700

    Add models for creating and reading messages

--- app/db/models.ts
+++ app/db/models.ts
@@ -1,5 +1,39 @@
+import { getImageAsDataUrl } from '~/utils/dashboard.server';
 import { db } from './db';
 
 export async function createUser(username: string, passHash: string) {
   return db.user.create({ data: { username, passHash } });
+}
+
+export async function createMessage(userId: string, image: string) {
+  return db.message.create({
+    data: { authorId: userId, path: image },
+  });
+}
+
+export async function readMessages(userId: string) {
+  const query = await db.message.findMany({
+    where: {
+      authorId: { equals: userId },
+    },
+    select: {
+      id: true,
+      path: true,
+      author: {
+        select: {
+          username: true,
+        },
+      },
+    },
+  });
+
+  const messages = query.map((entry) => {
+    return {
+      id: entry.id,
+      url: getImageAsDataUrl(entry.path),
+      username: entry.author.username,
+    };
+  });
+
+  return messages;
 }
