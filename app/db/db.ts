aacb9a259e5bd97c4d1bd6fe6ffcf28c43d34edd
Zac Paulson <zpaulson001@gmail.com>
2024-04-18 13:54:42 -0700

    Update db client to connect to turso

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,3 +1,18 @@
 import { PrismaClient } from '@prisma/client';
+import { PrismaLibSQL } from '@prisma/adapter-libsql';
+import { createClient } from '@libsql/client';
 
-export const db = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+export const db = new PrismaClient({
+  adapter,
+  log: ['query', 'info', 'warn', 'error'],
+});
+
+// import { PrismaClient } from '@prisma/client';
+
+// export const db = new PrismaClient({ log: ['query', 'info', 'warn', 'error'] });
