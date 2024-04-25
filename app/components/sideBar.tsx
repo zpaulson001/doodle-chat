77f1d570dcef060454f0714a28ed1b25df6d4af9
Zac Paulson <zpaulson001@gmail.com>
2024-04-25 15:34:08 -0700

    Add dropdown menu for accessing profile settings

--- app/components/sideBar.tsx
+++ app/components/sideBar.tsx
@@ -12,6 +12,13 @@ import { LogOut } from 'lucide-react';
 import NewMessagePopOver from './newMessagePopover';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { loader } from '~/routes/dashboard';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import EditProfilePicModal from './editProfilePicModal';
 
 type SideBarProps = {
   className?: string;
@@ -30,12 +37,24 @@ export default function SideBar({ className }: SideBarProps) {
         <ThreadList />
       </CardContent>
       <CardFooter className="flex-row gap-4 justify-center">
-        <Avatar className="">
-          <AvatarImage
-            src={`https://api.dicebear.com/8.x/thumbs/svg?scale=75&seed=${myUsername}`}
-          />
-          <AvatarFallback>{myUsername.substring(0, 1)}</AvatarFallback>
-        </Avatar>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <button className="focus-visible:outline-none">
+              <Avatar className="">
+                <AvatarImage
+                  src={`https://api.dicebear.com/8.x/thumbs/svg?scale=75&seed=${myUsername}`}
+                />
+                <AvatarFallback>{myUsername.substring(0, 1)}</AvatarFallback>
+              </Avatar>
+            </button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem className="active:bg-emerald-100">
+              Edit profile picture
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <EditProfilePicModal />
         <Form method="post">
           <Button name="intent" variant="outline" value="logout">
             <LogOut className="h-4 w-4" />
