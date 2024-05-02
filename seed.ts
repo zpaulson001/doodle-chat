5ee9157d610a6be23fcb938ba4e087312c212d42
Zac Paulson <zpaulson001@gmail.com>
2024-05-02 11:08:04 -0700

    Update for db seeding

---
+++ seed.ts
@@ -0,0 +1,5 @@
+import { createUser } from '~/db/models';
+
+await createUser('demouser', 'demouserpassword');
+await createUser('magichead', 'immagichead');
+await createUser('ovaltinejenkins', 'maybeitsovaltine');
